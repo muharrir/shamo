d80f5c2467871cecd58fe79da10ee0789468eff5
Muharrir <muharrir.@MacBook-Air.local>
2022-07-21 11:49:37 +0700

    fisrt commit

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/image_splash.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//NOTE: COLOR
+Color kPrimaryColor = const Color(0xff6C5ECF);
+Color kSecondaryColor = const Color(0xff38ABBE);
+Color kAlertColor = const Color(0xffED6363);
+Color kPriceColor = const Color(0xff2C96F1);
+Color kBackgroundColor1 = const Color(0xff1F1D2B);
+Color kBackgroundColor2 = const Color(0xff2B2937);
+Color kBackgroundColor3 = const Color(0xff242231);
+Color kPrimaryTextColor = const Color(0xffE1E1E1);
+Color kSecondaryTextColor = const Color(0xff999999);
+
+//NOTE: TEXTSTYLE
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: kPrimaryTextColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: kSecondaryTextColor,
+);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: kPriceColor,
+);
+
+//NOTE: FONTWEIGHT
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
